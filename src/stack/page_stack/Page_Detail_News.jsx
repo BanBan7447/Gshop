3a523fdce589ba153d0d7e6a90772e565373a3dd
BanBan <pn93948848@gmail.com>
2025-02-18 16:03:27 +0700

    Update News

--- src/stack/page_stack/Page_Detail_News.jsx
+++ src/stack/page_stack/Page_Detail_News.jsx
@@ -1,10 +1,38 @@
-import { View, Text, ScrollView, Image, FlatList, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image, FlatList, TouchableOpacity, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import Style_Detail_News from '../../styles/Style_Detail_News';
+import { api_getDetailNews } from '../../helper/ApiHelper';
+import colors from '../../styles/colors';
 
 const Page_Detail_News = (props) => {
     const { navigation, route } = props
-    const { newsItem } = route.params;
+    const { newsId } = route.params;
+
+    const [news, setNews] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const getDetailNews = async () => {
+        try {
+            const response = await api_getDetailNews(newsId);
+            setNews(response);
+        } catch (e) {
+            console.log(e);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getDetailNews();
+    }, [newsId]);
+
+    if (loading) {
+        return (
+            <View style={Style_Detail_News.container_loading}>
+                <ActivityIndicator size='large' color={colors.Red} />
+            </View>
+        )
+    }
 
     return (
         <ScrollView style={Style_Detail_News.container}>
@@ -18,19 +46,19 @@ const Page_Detail_News = (props) => {
                 <Text style={Style_Detail_News.text_navigation}>Tin tức</Text>
             </TouchableOpacity>
 
-            <Text style={Style_Detail_News.title_news}>{newsItem.title}</Text>
+            <Text style={Style_Detail_News.title_news}>{news.title}</Text>
 
 
-            <Text style={Style_Detail_News.date_news}>{newsItem.date}</Text>
+            <Text style={Style_Detail_News.date_news}>{news.date}</Text>
 
             <Image
-                source={{ uri: newsItem.images[0] }}
+                source={{ uri: news.images[0] }}
                 style={Style_Detail_News.thumbnails_news} />
 
-            <Text style={Style_Detail_News.content_news}>{newsItem.content}</Text>
+            <Text style={Style_Detail_News.content_news}>{news.content}</Text>
 
             <FlatList
-                data={newsItem.images.slice(1)} // Lấy tất cả ảnh từ vị trí thứ 2
+                data={news.images.slice(1)} // Lấy tất cả ảnh từ vị trí thứ 2
                 keyExtractor={(item, index) => index.toString()}
                 renderItem={({ item }) => (
                     <Image
--- src/stack/tab_stack/Page_News.jsx
+++ src/stack/tab_stack/Page_News.jsx
@@ -1,34 +1,61 @@
-import { View, Text, FlatList, Image, ScrollView, TextInput, TouchableOpacity } from 'react-native'
+import { View, Text, FlatList, Image, ScrollView, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native'
 import React, { useContext, useEffect, useState } from 'react'
 import colors from '../../styles/colors'
 import Styles_News from '../../styles/Styles_News'
 import { AppContext } from '../../context'
+import { api_getNews } from '../../helper/ApiHelper'
+import Style_Detail_News from '../../styles/Style_Detail_News'
 
 const Page_News = (props) => {
-  const { navigation } = props
+  const { navigation } = props;
 
-  // Data tin tức
-  const { news } = useContext(AppContext);
+  const [news, setNews] = useState([]);
   const [searchText, setSearchText] = useState('');
+  const [loading, setLoading] = useState(true);
 
-  // Lọc tin tức theo tiêu đề
-  const filterNews = news.filter(item =>
-    item.title.toLowerCase().includes(searchText.toLowerCase())
-  );
+  // Lấy data tin tức
+  const get_News = async () => {
+    try {
+      const response = await api_getNews();
+      setNews(response);
+    } catch (e) {
+      console.log(e);
+    }finally{
+      setLoading(false);
+    }
+  };
+
+  // Gọi get_News
+  useEffect(() => {
+    get_News();
+  }, [])
 
   const renderNews = ({ item }) => {
-    const { images, title, date } = item;
+    const { images, title, date, _id } = item;
 
     return (
       <TouchableOpacity
         style={Styles_News.container_news}
-        onPress={() => navigation.navigate('DetailNews', { newsItem: item })}>
+        onPress={() => navigation.navigate('DetailNews', { newsId: _id })}>
         <Image
           source={{ uri: images[0] }}
           style={Styles_News.thumbnails_news} />
         <Text style={Styles_News.title_news}>{title}</Text>
         <Text style={Styles_News.date_news}>{date}</Text>
       </TouchableOpacity>
+    )
+  }
+
+  // Lọc tin tức theo tiêu đề
+  const filterNews = news.filter(item =>
+    item.title.toLowerCase().includes(searchText.toLowerCase())
+  );
+
+  if (loading) {
+    return (
+      <View style={Style_Detail_News.container_loading}>
+        <ActivityIndicator size="large" color={colors.Red} />
+      </View>
     )
   }
 
